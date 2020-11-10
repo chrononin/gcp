ab81eaaf190a8112dd23e1f7a34b8166a25aff26
devbox <devbox@devbox-VirtualBox>
2020-11-10 18:04:51 +0530

    initial commit

---
+++ gcppub/gcppub/src/main/java/com/example/gcp/GcppubApplication.java
@@ -0,0 +1,45 @@
+package com.example.gcp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@SpringBootApplication
+public class GcppubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcppubApplication.class, args);
+	}
+	
+	@MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+    public interface PubsubOutboundGateway {
+
+            void sendToPubsub(String text);
+    }
+	@Bean
+    @ServiceActivator(inputChannel = "pubsubOutputChannel")
+    public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
+            return new PubSubMessageHandler(pubsubTemplate, "MyPubSub");
+    }
+	
+	@Autowired
+    private PubsubOutboundGateway messagingGateway;
+
+    @PostMapping("/postMessage")
+    public RedirectView postMessage(@RequestParam("message") String message) {
+            this.messagingGateway.sendToPubsub(message);
+            return new RedirectView("/");
+    }	
+
+}
